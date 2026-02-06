9a4ccb2ccf2074c0522823b40178a357cd9fb4ca
liu <2102033535@qq.com>
2026-02-06 10:30:47 +0800

    first commit

---
+++ build/CMakeFiles/vulkan.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vulkan.dir/link.d"
+  "CMakeFiles/vulkan.dir/src/main.cpp.o"
+  "CMakeFiles/vulkan.dir/src/main.cpp.o.d"
+  "vulkan"
+  "vulkan.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vulkan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
