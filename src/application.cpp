6060256499cea61a97f5a284efca8421a6d3d766
liu <2102033535@qq.com>
2026-02-06 12:06:46 +0800

    append initialize function

--- src/application.cpp
+++ src/application.cpp
@@ -8,4 +8,15 @@ Application::Application(uint32_t width, uint32_t height, const char *prefix)
 
 Application::~Application() {}
 
-void Application::run() {}
+void Application::run()
+{
+    void initWindow();
+    void initVulkan();
+    void mainLoop();
+    void cleanup();
+}
+void Application::initWindow() {}
+void Application::initVulkan() {}
+void Application::mainLoop() {}
+void Application::cleanup() {}
+
